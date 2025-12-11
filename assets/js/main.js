bce7d2aceb94e07cc7bd1183186e4bb14dfb78f8
xscapd <xscapd@gmail.com>
2025-12-11 20:28:27 +0000

    Made overlay disappear on document load.

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+	let fadeTarget = document.getElementById("loadOverlay")
+	let fadeEffect = setInterval(function () {
+		if (!fadeTarget.style.opacity) {
+			fadeTarget.style.opacity = 1;
+		}
+		if (fadeTarget.style.opacity > 0) {
+			fadeTarget.style.opacity -= 0.09;
+		} else {
+			clearInterval(fadeEffect);
+			fadeTarget.remove();
+		}
+	}, 10);
+
+})
+
